54bb00cbd4ba985bacaf9d37e87caf2a04669e5d
lglputong <lance435@gmail.com>
2014-05-07 10:19:20 +0800

    Midterms v2

--- server.rb
+++ server.rb
@@ -18,3 +18,10 @@ end
 get '/show/:id' do
 	erb :show
 end
+
+post '/save' do
+	last_expense = expenses.last
+	expense = Expense.new(last_expense.id += 1, :some_item, :some_amount)
+	expenses << expense
+	erb :index
+end
